b5a0ae4967086919836a5f401749c3e7d579b953
echavis <echavis@umich.edu>
2019-07-24 20:40:27 -0400

    Live reload server on file save

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]]
   :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler interview-accelerator.handler/app}
+  :ring {:handler interview-accelerator.handler/app
+         :auto-reload? true
+         :auto-refresh? true}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]]}})
